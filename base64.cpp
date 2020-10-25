2f5b6cbce6fda256757ea11b19db9033f6a4f156
Daze <dazehere@yandex.com>
2020-10-25 18:10:11 +0545

    Update

--- base64.cpp
+++ base64.cpp
@@ -1,53 +1,23 @@
 #include "base64.h"
 
-inline unsigned int b64(char a_char)
+unsigned char* Base64::decode()
 {
-    if(a_char >= 0x30 && a_char <= 0x39)
-    {
-        return (a_char - 0x30 + 0x34);
-    }
-    else if(a_char >= 0x41 && a_char <= 0x5A)
-    {
-        return (a_char - 0x41);
-    }
-    else if(a_char >= 0x61 && a_char <= 0x7A)
-    {
-        return (a_char - 0x61 + 0x1A);
-    }
-    else if(a_char == '+')
-    {
-        return 0x3E;
-    }
-    else if(a_char == '/')
-    {
-        return 0x3F;
-    }
-    else
-    {
-        return 0xFF;
-    }
-}
-
-string Base64::toBase64()
-{
-    return toBase64(source_filename);
-}
-
-unsigned char* Base64::toBinary(string s_b64)
-{
-    buffer_size = s_b64.length()*3/4;
+    int paddding_size = 0;
+    paddding_size += (base64_string[base64_string.length()-1]=='=')?1:0;
+    paddding_size += (base64_string[base64_string.length()-2]=='=')?1:0;
+    buffer_size = base64_string.length()* 3/4 - paddding_size;
     buffer = new unsigned char[buffer_size];
     cout << buffer_size << endl;
-    for(int i = 0; i < s_b64.length(); i+=4)
+    for(int i = 0; i < base64_string.length(); i+=4)
     {
         int b_index = i*3/4;
         unsigned char b64_vals[4];
         unsigned char bytes[3];
 
-        b64_vals[0] = b64(s_b64[i]);
-        b64_vals[1] = b64(s_b64[i+1]);
-        b64_vals[2] = b64(s_b64[i+2]);
-        b64_vals[3] = b64(s_b64[i+3]);
+        b64_vals[0] = b64(base64_string[i]);
+        b64_vals[1] = b64(base64_string[i+1]);
+        b64_vals[2] = b64(base64_string[i+2]);
+        b64_vals[3] = b64(base64_string[i+3]);
 
         buffer[b_index] = (b64_vals[0]<<0x2)^(b64_vals[1]>>0x4);
         buffer[b_index+1] = ((b64_vals[1]&0xF)<<0x4)^(b64_vals[2]>>0x2);
@@ -56,24 +26,44 @@ unsigned char* Base64::toBinary(string s_b64)
     return buffer;
 }
 
-void Base64::toBinary(string s_b64, string filename)
+/* string Base64::decode_to_string()
+{
+    decode();
+    string return_string((char *)buffer);
+    return_string = return_string.substr(0,buffer_size);
+    return return_string;
+} */
+
+void Base64::decode(string filename)
 {
     ofstream output;
     output.open(filename, ios::out | ios::binary);
-    toBinary(s_b64);
+    decode();
     output.write((char *)buffer,buffer_size);
     output.close();
 }
 
-string Base64::toBase64(string filename)
+string Base64::encode()
 {
-    file.open(filename,ios::in | ios::binary);
-    file.seekg(0,ios::end);
-    buffer_size = file.tellg();
-    file.seekg(0,ios::beg);
+    if(input_mode==Base64::FileMode)
+    {
+        ifstream file;
+        file.open(source_filename,ios::in | ios::binary);
+        file.seekg(0,ios::end);
+        buffer_size = file.tellg();
+        file.seekg(0,ios::beg);
 
-    buffer = new unsigned char[buffer_size];
-    file.read((char *)buffer, buffer_size);
+        buffer = new unsigned char[buffer_size];
+        file.read((char *)buffer, buffer_size);
+        file.close();
+    }
+
+    else if(input_mode==Base64::TextMode)
+    {
+        buffer_size = source_string.length();
+        buffer = new unsigned char[buffer_size];
+        buffer = (unsigned char *)source_string.c_str();
+    }
 
     int padding_length = buffer_size % 3;
 
@@ -89,35 +79,35 @@ string Base64::toBase64(string filename)
         char character;
         for(int j = 0; j <= 3; j++)
         {
-            if(separatedBits[j] <= 25)
+            if(separatedBits[j] <= 0x19)
             {
                 character = 'A' + separatedBits[j];
             }
-            else if(separatedBits[j]>=26 && separatedBits[j] <= 51)
+            else if(separatedBits[j] >= 0x1A && separatedBits[j] <= 0x33)
             {
-                character = 'a' + separatedBits[j] - 26;
+                character = 'a' + separatedBits[j] - 0x1A;
             }
-            else if(separatedBits[j]>=52 && separatedBits[j] <= 61)
+            else if(separatedBits[j] >= 0x34 && separatedBits[j] <= 0x3D)
             {
-                character = '0' + separatedBits[j] - 52;
+                character = '0' + separatedBits[j] - 0x34;
             }
-            else if(separatedBits[j] == 62)
+            else if(separatedBits[j] == 0x3E)
             {
                 character = '+';
             }
-            else if(separatedBits[j] == 63)
+            else if(separatedBits[j] == 0x3F)
             {
                 character = '/';
             }
-            finalBase += character;
+            base64_string += character;
         }
 
     }
     if(padding_length==1){
-        finalBase = finalBase.substr(0,finalBase.length()-2) + "==";
+        base64_string = base64_string.substr(0,base64_string.length()-2) + "==";
     }
     else if(padding_length==2){
-        finalBase = finalBase.substr(0,finalBase.length()-1) + "=";
+        base64_string = base64_string.substr(0,base64_string.length()-1) + "=";
     }
-    return finalBase;
+    return base64_string;
 }
--- base64.h
+++ base64.h
@@ -1,24 +1,69 @@
 #include <fstream>
-#include <deque>
 #include <iostream>
 
 using namespace std;
 
+typedef unsigned int mode;
+
 class Base64
 {
     private:
-        ifstream file;
         long buffer_size;
         string source_filename;
-        string finalBase;
+        string source_string;
+        string base64_string;
         unsigned char *buffer;
+        mode input_mode;
+
     public:
-        Base64(string a_filename)
+        static const mode TextMode = 0;
+        static const mode FileMode = 1;
+        static const mode DecodeMode = 3;
+
+        Base64(string a_source, mode a_mode = Base64::TextMode)
         {
-            source_filename = a_filename; 
+            input_mode = a_mode;
+            if(a_mode == Base64::FileMode){
+                source_filename = a_source; 
+            }
+            else if(a_mode == Base64::DecodeMode){
+                base64_string = a_source;
+            }
+            else{
+                source_string = a_source;
+            }          
         }
-        string toBase64(string);
-        string toBase64();
-        unsigned char* toBinary(string);
-        void toBinary(string, string);
+
+        string encode();
+        unsigned char* decode();
+        void decode(string);
+        string decode_to_string();
+
+        inline unsigned int b64(char a_char)
+        {
+            if(a_char >= 0x30 && a_char <= 0x39)
+            {
+                return (a_char - 0x30 + 0x34);
+            }
+            else if(a_char >= 0x41 && a_char <= 0x5A)
+            {
+                return (a_char - 0x41);
+            }
+            else if(a_char >= 0x61 && a_char <= 0x7A)
+            {
+                return (a_char - 0x61 + 0x1A);
+            }
+            else if(a_char == '+')
+            {
+                return 0x3E;
+            }
+            else if(a_char == '/')
+            {
+                return 0x3F;
+            }
+            else
+            {
+                return 0xFF;
+            }
+        }
 };
